0b83fa9bfde1680208ba4e740f7269adf2d7905e
Adriano da Rosa Ferreira <adrianorosaferreira@gmail.com>
2018-10-21 16:26:57 -0300

    Added example of align items

---
+++ flex-container/align-items/align-items.js
@@ -0,0 +1,10 @@
+function setAlignItems(align) {
+    var container=document.getElementById('container'); 
+        container.style.WebkitAlignItems = align;
+        container.style.alignItems = align; 
+}
+function setStyleVisibility(visibility) {
+    var line = document.getElementById('line'); 
+        line.style.visibility = visibility;
+}
+
