2a4979d5ce1499d27b97c1bb3f7cf817815a2545
Adriano Ferreira <adrianorosaferreira@gmail.com>
2018-10-25 17:23:54 -0300

    Adding property example of 'order'

--- flex-item/order/order.js
+++ flex-item/order/order.js
@@ -1,12 +1,16 @@
-function setOrder(element,order) {
-    var item = document.getElementById(element); 
-        item.style.order = order ;
-        item.style.backgroundColor= 'red' ;
+function setOrder(array) {
+    colorDefault()
+    array.forEach(element => {
+        var item = document.getElementById(element[0]);
+        item.style.order = element[1];
+        item.style.backgroundColor = 'red';
+    });
 }
+
 function colorDefault() {
     items = document.getElementsByClassName('item')
     for (let index = 0; index < items.length; index++) {
-        items[index].style.backgroundColor = 'white';       
+        items[index].style.backgroundColor = 'white';
+        items[index].style.order = 'unset';
     }
 }
-
